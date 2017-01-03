4fc46c5f2130af936c757f9a66f4b553e17b5581
卫旗 <429265216@qq.com>
2017-01-03 17:22:33 +0800

    js修改

--- js/template.js
+++ js/template.js
@@ -2,135 +2,161 @@
  * Created by Administrator on 2016/12/30.
  * author :卫旗
  */
-//定义模板
+//定义模板，我的实验室：每个系列的模板
 Vue.component("example", {
-    template: "#example-demo",
+    template: "#template-example-demo",
     props: ["a"],
+});
+//前端社区模板
+Vue.component("content-box",{
+    template:"#template-content-shequ",
+    props:["content"]
+});
+Vue.component("template-example",{
+    template:"#template-content-example",
+    prop:["exams","content"]
 });
 var app = new Vue({
     el: "#app",
     data: {
-        //国内综合
-        zonghe: [
+        //前端社区模板对象
+        contents:[
+            //国内综合
             {
-                src: "https://www.w3ctech.com/", des: "w3ctech"
+                title:"国内综合",
+                type:[
+                    {
+                        src: "https://www.w3ctech.com/", des: "w3ctech"
+                    },
+                    {
+                        src: "http://www.w3cplus.com/", des: "w3cplus"
+                    },
+                    {
+                        src: "http://www.qdfuns.com/", des: "w3cfuns"
+                    }
+                    ,
+                    {
+                        src: "http://www.html-js.com/", des: "前端乱炖"
+                    }
+                    ,
+                    {
+                        src: "https://www.qianduan.net/", des: "前端观察"
+                    }
+                    ,
+                    {
+                        src: "https://segmentfault.com/", des: "segmentfault"
+                    }
+                    ,
+                    {
+                        src: "http://www.html5tricks.com/", des: "html5tricks"
+                    }
+                    ,
+                    {
+                        src: "http://isux.tencent.com/", des: "腾讯ISUX"
+                    }
+                    ,
+                    {
+                        src: "http://efe.baidu.com/", des: "百度EFE"
+                    }
+                    ,
+                    {
+                        src: "http://www.75team.com/weekly/", des: "奇舞团"
+                    }
+                    ,
+                    {
+                        src: "http://www.yyyweb.com/", des: "前端里"
+                    }
+                    ,
+                    {
+                        src: "http://div.io/#/welcome", des: "div.io"
+                    }
+                    ,
+                    {
+                        src: "http://www.css88.com/", des: "web前端开发"
+                    }
+                    ,
+                    {
+                        src: "http://caibaojian.com/", des: "前端开发博客"
+                    }
+                    ,
+                    {
+                        src: "http://wx.h5.vc/", des: "爱奇舞-H5.vc"
+                    }
+                    ,
+                    {
+                        src: "http://www.kancloud.cn/explore", des: "看云"
+                    }
+                    ,
+                    {
+                        src: "http://www.hubwiz.com/", des: "汇智网"
+                    }
+                    ,
+                    {
+                        src: "http://blog.jobbole.com/", des: "伯乐在线"
+                    }
+                ]
             },
+            //问答社区
             {
-                src: "http://www.w3cplus.com/", des: "w3cplus"
-            }
-            ,
-            {
-                src: "http://www.qdfuns.com/", des: "w3cfuns"
-            }
-            ,
-            {
-                src: "http://www.html-js.com/", des: "前端乱炖"
-            }
-            ,
-            {
-                src: "https://www.qianduan.net/", des: "前端观察"
-            }
-            ,
-            {
-                src: "https://segmentfault.com/", des: "segmentfault"
-            }
-            ,
-            {
-                src: "http://www.html5tricks.com/", des: "html5tricks"
-            }
-            ,
-            {
-                src: "http://isux.tencent.com/", des: "腾讯ISUX"
-            }
-            ,
-            {
-                src: "http://efe.baidu.com/", des: "百度EFE"
-            }
-            ,
-            {
-                src: "http://www.75team.com/weekly/", des: "奇舞团"
-            }
-            ,
-            {
-                src: "http://www.yyyweb.com/", des: "前端里"
-            }
-            ,
-            {
-                src: "http://div.io/#/welcome", des: "div.io"
-            }
-            ,
-            {
-                src: "http://www.css88.com/", des: "web前端开发"
-            }
-            ,
-            {
-                src: "http://caibaojian.com/", des: "前端开发博客"
-            }
-            ,
-            {
-                src: "http://wx.h5.vc/", des: "爱奇舞-H5.vc"
-            }
-            ,
-            {
-                src: "http://www.kancloud.cn/explore", des: "看云"
-            }
-            ,
-            {
-                src: "http://www.hubwiz.com/", des: "汇智网"
-            }
-            ,
-            {
-                src: "http://blog.jobbole.com/", des: "伯乐在线"
-            }
-        ],
-        //问答社区
-        wenda: [
-            {
-                src: "https://www.zhihu.com/", des: "知乎"
-            }
-        ],
-        //每日最新
-        zuixin: [
-            {src: "http://gold.xitu.io/", des: "稀土掘金"},
-            {src: "https://toutiao.io/", des: "开发者头条"},
-            {src: "http://geek.csdn.net/", des: "极客头条"},
-            {src: "http://hao.caibaojian.com/", des: "开发头条"}
-        ],
-        //前端大神
-        dashens: [
-            {
-                src: "http://www.liaoxuefeng.com/",
-                des: "廖雪峰"
+                title:"问答社区",
+                type:[
+                    {
+                        src: "https://www.zhihu.com/", des: "知乎"
+                    }
+                ]
             },
+            //每日最新
             {
-                src: "http://www.ruanyifeng.com/blog/",
-                des: "阮一峰"
+                title:"每日最新",
+                type:[
+                    {src: "http://gold.xitu.io/", des: "稀土掘金"},
+                    {src: "https://toutiao.io/", des: "开发者头条"},
+                    {src: "http://geek.csdn.net/", des: "极客头条"},
+                    {src: "http://hao.caibaojian.com/", des: "开发头条"}
+                ]
             },
+            //前端博客
             {
-                src: "http://www.zhangxinxu.com/wordpress/",
-                des: "张鑫旭"
+                title:"前端博客",
+                type:[
+                    {
+                        src: "http://www.liaoxuefeng.com/",
+                        des: "廖雪峰"
+                    },
+                    {
+                        src: "http://www.ruanyifeng.com/blog/",
+                        des: "阮一峰"
+                    },
+                    {
+                        src: "http://www.zhangxinxu.com/wordpress/",
+                        des: "张鑫旭"
+                    },
+                    {
+                        src: "https://yuguo.us/weblog/",
+                        des: "余果"
+                    },
+                    {
+                        src: "http://www.cnblogs.com/Darren_code/",
+                        des: "聂微东"
+                    },
+                    {
+                        src: "http://jiongks.name/all-slides/",
+                        des: "勾三股四"
+                    },
+                    {
+                        src: "http://www.cnblogs.com/rubylouvre/",
+                        des: "司徒正美"
+                    }
+                ]
             },
+            //前端教程
             {
-                src: "https://yuguo.us/weblog/",
-                des: "余果"
-            },
-            {
-                src: "http://www.cnblogs.com/Darren_code/",
-                des: "聂微东"
-            },
-            {
-                src: "http://jiongks.name/all-slides/",
-                des: "勾三股四"
-            },
-            {
-                src: "http://www.cnblogs.com/rubylouvre/",
-                des: "司徒正美"
-            },
-        ],
-        //前端教程
-        jiaocheng:[
-            {
-                src:"http://www.imooc.com/",des:"慕课网"
+                title:"前端教程",
+                type:[
+                    {
+                        src:"http://www.imooc.com/",des:"慕课网"
+                    }
+                ]
             }
         ],
         //CSS实例
@@ -164,6 +190,12 @@ var app = new Vue({
                 eSrc: "example/CSS3/photo.html",
                 eHead: "3D相册",
                 eDesc: "利用CSS3的3D转换制作的一个3D相册，再使用动画动起来"
+            },
+            {
+                eClass: "photo_3d",
+                eSrc: "example/CSS3/3d.html",
+                eHead: "CSS3 3D炫酷旋转动画",
+                eDesc: "两个3D立方体，hover状态时第一个散开"
             }
         ],
         //canvas实例
